e51ecd7b32da2297e567d386cca6ac2a0971968a
Priyanshu Kumar Sinha <79042642+priyanshukumarsinha@users.noreply.github.com>
2025-10-21 20:39:10 +0000

    refactor ast

--- parser/ast.h
+++ parser/ast.h
@@ -1,65 +1,106 @@
 #ifndef AST_H
 #define AST_H
 
-#include<string>
+#include <string>
+#include <vector>
+#include <memory>
+#include <variant>
+#include "../lexer/token.h"
 
-// Base Node
-// Every other node will be a derived class of ASTNode
+// Base class for all AST Nodes
+// it makes it easier to keep different types of Nodes 
+// like for statement, statement := declaration | assignment | if_statement
+// now we need to store any of these three
+// one option is to use variant
+// but we are going to implement Base Node option here
+// This is like S (Start Symbol)
+// S:= ProgramNode
 struct ASTNode{
-    // the virtual destructor helps delete derived classes safely
     virtual ~ASTNode() = default;
+    // somehow it helps in cleanup when deleting derived classes such as declaration, assignment, etc...
 };
 
-// ProgramNode := {controlNode}
+// ProgramNode:= { ControlNode }
 struct ProgramNode : ASTNode{
+    // there can be more than one controlnodes
+    // hence vector, but yeah not vector of ControlNodes
+    // Not vector<ControlNodes>
+    // we need to store pointer of these 
+    // that too unique_ptr, don't know why
     std::vector<std::unique_ptr<ControlNode>> controlBlocks;
 };
 
-// control Engine {
-//     set speed 100;
-//     if (speed > 50) { ... }
-// }
-// ControlNode := "control" <name> {statements}
+// ControlNode:= "control" IDENTIFIER "{" { statement } "}"
 struct ControlNode : ASTNode{
-    std::string name; // <name>
-    std::vector<std::unique_ptr<ASTNode>> statements; // {statements}
+    // IDENTIFIER is name of the control block
+    std::string name;
+    
+    // and a vector of all the statements
+    std::vector<std::unique_ptr<StatementNode>> statements;
 };
 
-using StatementVariant = std::variant<
-    std::unique_ptr<VarDeclNode>,
-    std::unique_ptr<SetNode>,
-    std::unique_ptr<IfNode>,
->;
-
-// Statements := VarDeclNode | setNode | ifNode | ExpressionNode
+// StatementNode:= VarDeclNode | AssignmentNode | IfNode
+// Now again we have a base class for all these derived classes
 struct StatementNode : ASTNode{
-    StatementVariant statement;  // only one active at a time
+    virtual ~StatementNode() = default;
 };
 
-// VarDeclNode := type name;
-struct VarDeclNode : ASTNode{
-    TokenType type; // datatype : INT, FLOAT, BOOL
-    std::string name; // variable
+// VarDeclNode:= TYPE IDENTIFIER ";"
+struct VarDeclNode : StatementNode{
+    TokenType type; // INT_TYPE, FLOAT_TYPE, BOOL_TYPE
+    std::string identifier;
 };
 
-// SetNode := set <identifier> <expression>;
-struct SetNode : ASTNode{
-    std::string name; // <identifier>
-    std::unique_ptr<ASTNode> expr; // ExpressionNode
+// AssignmentNode := "set" IDENTIFIER expression ";"
+struct AssignmentNode : StatementNode{
+    std::string identifier;
+    std::unique_ptr<ExpressionNode> expression;
 };
 
-// IfNode := if (ComparisionExpression) { statements }
-struct IfNode : ASTNode{
-    std::unique_ptr<ASTNode> condition;
-    std::vector<std::unique_ptr<ASTNode>> statements;
+// ExpressionNode := TermNode op TermNode
+// can be implemented using a binary tree
+struct ExpressionNode : ASTNode{
+    std::unique_ptr<TermNode> left;
+    TokenType op;
+    std::unique_ptr<TermNode> right;
 };
 
-// Expression := <term> {operator} <term>
-struct ExpressionNode : ASTNode {
-    std::string value;
-    std::string op;
-    std::unique_ptr<ExpressionNode> left;
-    std::unique_ptr<ExpressionNode> right;
+// TermNode:= FactorNode
+struct TermNode : ASTNode{
+    std::unique_ptr<FactorNode> factor;
 };
+
+// FactorNode:= IDENTIFIER | literal | "(" expression ")" 
+struct FactorNode {
+    virtual ~FactorNode() = default;
+};
+
+struct IdentifierNode : FactorNode {
+    std::string identifier;
+};
+
+struct LiteralNode : FactorNode {
+    std::variant<std::monostate, int, float, bool> literalValue;
+    TokenType literalType = TokenType::EOF_TOKEN;
+};
+
+struct ParenExpressionNode : FactorNode {
+    std::unique_ptr<struct ExpressionNode> expression; // for parentheses
+};
+
+// IfNode := "if" ConditionNode "{" { statement } "}"
+struct IfNode : StatementNode{
+    std::unique_ptr<ConditionNode> condition;
+    std::vector<std::unique_ptr<StatementNode>> statements;
+};
+
+
+// ConditionNode := ExpressionNode comparisionOp ExpressionNode
+struct ConditionNode{
+    std::unique_ptr<struct ExpressionNode> left;
+    TokenType comparisonOp; // GREATER or EQUAL_EQUAL
+    std::unique_ptr<struct ExpressionNode> right;
+};
+
 
 #endif // AST_H
